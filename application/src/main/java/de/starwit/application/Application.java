e0665ef4fbb3e5ae9273641f8db20b7752028b21
Anett Hübner <anett_huebner@gmx.de>
2025-07-15 17:04:32 +0200

    added application

---
+++ application/src/main/java/de/starwit/application/Application.java
@@ -0,0 +1,49 @@
+package de.starwit.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@SpringBootApplication(scanBasePackages = {
+        "de.starwit.rest",
+        "de.starwit.service",
+        "de.starwit.persistence",
+        "de.starwit.application.config"
+}, exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(new Class[] { Application.class }, args);
+    }
+
+    @Bean
+    public ObjectMapper mapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("filterName", SimpleBeanPropertyFilter.filterOutAllExcept("name"));
+        filterProvider.addFilter("filterId", SimpleBeanPropertyFilter.filterOutAllExcept("id"));
+        filterProvider.addFilter("filterIdName", SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "title"));
+        mapper.setFilterProvider(filterProvider);
+        return mapper;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(3000);
+
+        restTemplate.setRequestFactory(requestFactory);
+        return restTemplate;
+    }
+}
