baf8f08e6b18f81f73761e43db6901e05b45c082
Anett Hübner <anett_huebner@gmx.de>
2025-09-03 16:20:22 +0200

    corrected translation

--- rest/src/main/java/de/starwit/rest/controller/DetectionCountController.java
+++ rest/src/main/java/de/starwit/rest/controller/DetectionCountController.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
