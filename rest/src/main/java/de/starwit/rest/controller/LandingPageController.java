51c6bd56129b0102c66f3a6fb53c5bc9ebe48b52
ztarbug <markus.zarbock@starwit.de>
2025-09-15 12:19:25 +0200

    make landing page configurable

--- rest/src/main/java/de/starwit/rest/controller/LandingPageController.java
+++ rest/src/main/java/de/starwit/rest/controller/LandingPageController.java
@@ -2,42 +2,83 @@ package de.starwit.rest.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@ConfigurationProperties(prefix = "landing")
+class LandingProperties {
+    private Map<String, EndPointData> endpoints;
+
+    public Map<String, EndPointData> getEndpoints() {
+        return endpoints;
+    }
+
+    public void setEndpoints(Map<String, EndPointData> endpoints) {
+        this.endpoints = endpoints;
+    }
+}
+
+class EndPointData {
+    private String name;
+    private String endpoint;
+    private String image;
+    private int order;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+}
 
 @RestController
 @RequestMapping(path = "${rest.base-path}/landing-page")
 public class LandingPageController {
 
-    @Value("${landing.wastemanagement.endpoint}")
-    private String wasteManagementUrl;
+    private static final Logger log = LoggerFactory.getLogger(LandingPageController.class);
 
-    @Value("${landing.parking.endpoint}")
-    private String parkingUrl;
+    private final LandingProperties landingProperties;
 
-    @Value("${landing.trafficlive.endpoint}")
-    private String trafficLiveUrl;
-
-    @Value("${landing.trafficanalysis.endpoint}")
-    private String trafficAnalysisUrl;
-
-    @Value("${landing.assetmanagement.endpoint}")
-    private String assetManagementUrl;
-
-    @Value("${landing.publicsafety.endpoint}")
-    private String publicSafetyUrl;
+    public LandingPageController(LandingProperties landingProperties) {
+        this.landingProperties = landingProperties;
+    }
 
     @GetMapping
-    public Map<String, String> getEndpoints() {
-        return Map.of(
-                "wastemanagement", wasteManagementUrl,
-                "parking", parkingUrl,
-                "trafficlife", trafficLiveUrl,
-                "trafficanalysis", trafficAnalysisUrl,
-                "assetmanagement", assetManagementUrl,
-                "publicsafety", publicSafetyUrl);
+    public Map<String, EndPointData> getEndpoints() {
+        log.info(landingProperties.getEndpoints().toString());
+        return landingProperties.getEndpoints();
     }
 
 }
--- webclient/app/src/app/features/landing/LandingPage.jsx
+++ webclient/app/src/app/features/landing/LandingPage.jsx
@@ -2,21 +2,7 @@ import {useEffect, useState, useMemo} from "react";
 import {Grid, Card, CardActionArea, CardMedia, CardContent, Typography, Link} from "@mui/material";
 import {useTranslation} from "react-i18next";
 
-import CityHall from "../../assets/landing/city-hall.jpg";
-import TrafficFlow from "../../assets/landing/TrafficFlow.png";
-import StreetMap from "../../assets/landing/streetmap-sh.png";
-import Police from "../../assets/landing/polizei_alexander-fox_pixabay.jpg";
-
 import LandingPageRest from "../../services/LandingPageRest";
-
-const tiles = [
-    {title: "wastemanagement", image: CityHall},
-    {title: "trafficlive", image: TrafficFlow},
-    {title: "trafficanalysis", image: TrafficFlow},
-    {title: "parking", image: TrafficFlow},
-    {title: "assetmanagement", image: StreetMap},
-    {title: "publicsafety", image: Police},
-];
 
 export default function StartPage() {
     const {t, i18n} = useTranslation();
@@ -32,29 +18,29 @@ export default function StartPage() {
             if (response.data == null) {
                 return;
             }
+            Object.keys(response.data).map((entry, index) => {
+                console.log(response.data[entry]);
+            });
             setLandingPageData(response.data);
         });
     }
 
     return (
         <Grid container spacing={3} justifyContent="center" rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
-            {tiles.map((entry, index) => (
-                <Grid size={{xs: 12, sm: 6, md: 3}} key={index}>
+            {Object.keys(landingPageData).map((entry, index) => (
+                <Grid size={{xs: 12, sm: 6, md: 4}} key={index}>
                     <Card sx={{borderRadius: 3, boxShadow: 4}}>
-                        <CardActionArea>
+                        <CardActionArea onClick={() => window.open(landingPageData[entry].endpoint, '_blank')}>
                             <CardMedia
                                 component="img"
                                 height="250"
-                                image={entry.image}
-                                alt={t(entry.title)}
+                                image={'landing/' + landingPageData[entry].image}
+                                alt={t(landingPageData[entry].name)}
                             />
                             <CardContent>
                                 <Typography align="center" variant="h6">
-                                    {t(`landing.${entry.title}`)}
+                                    {t(`landing.${landingPageData[entry].name}`)}
                                 </Typography>
-                                <Link href="#" variant="body2">
-                                    {t(`landing.${entry.title}`)}
-                                </Link>
                             </CardContent>
                         </CardActionArea>
                     </Card>
