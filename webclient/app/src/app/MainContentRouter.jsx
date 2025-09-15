85479301fe363808330b7edec532d5d71026ca1f
ztarbug <markus.zarbock@starwit.de>
2025-09-15 13:35:10 +0200

    make landing page default target

--- webclient/app/src/app/MainContentRouter.jsx
+++ webclient/app/src/app/MainContentRouter.jsx
@@ -8,10 +8,10 @@ import DetectionOverview from "./features/detection/DetectionOverview";
 function MainContentRouter() {
     return (
         <Routes>
-            <Route path="/" element={<Layout disabled={true}><DetectionOverview /></Layout>} />
+            <Route path="/" element={<LandingLayout disabled={true}><LandingPage /></LandingLayout>} />
+            <Route path="/landing" element={<LandingLayout disabled={true}><LandingPage /></LandingLayout>} />
             <Route path="/status" element={<Layout disabled={true}><DetectionOverview /></Layout>} />
             <Route path="/vehicles" element={<Layout disabled={true}><Vehicles /></Layout>} />
-            <Route path="/landing" element={<LandingLayout disabled={true}><LandingPage /></LandingLayout>} />
             <Route path="/logout" component={() => {
                 window.location.href = window.location.pathname + "api/user/logout";
                 return null;
