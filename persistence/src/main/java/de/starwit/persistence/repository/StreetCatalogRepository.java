c68feda2e32eb0f647d2f94b946506636e975150
Anett Hübner <anett_huebner@gmx.de>
2025-11-18 14:35:22 +0100

    add detectioncount by streetname rest service

--- persistence/src/main/java/de/starwit/persistence/repository/StreetCatalogRepository.java
+++ persistence/src/main/java/de/starwit/persistence/repository/StreetCatalogRepository.java
@@ -2,7 +2,10 @@ package de.starwit.persistence.repository;
 
 import java.util.List;
 
+import org.locationtech.jts.geom.Geometry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.starwit.persistence.entity.StreetCatalogEntity;
@@ -11,4 +14,8 @@ import de.starwit.persistence.entity.StreetCatalogEntity;
 public interface StreetCatalogRepository extends JpaRepository<StreetCatalogEntity, Long> {
 
     public List<StreetCatalogEntity> findByCity(String city);
+
+    @Query(value = "SELECT st_buffer(street_path, 0.0005) FROM street_catalog WHERE street_name like :streetname LIMIT 1", nativeQuery = true)
+    Geometry findStreet(@Param("streetname") String streetname);
+
 }
