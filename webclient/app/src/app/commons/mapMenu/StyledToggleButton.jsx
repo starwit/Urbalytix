4d4c8ef385361e7f06ba9d113909f845211a9fdf
Anett Hübner <anett_huebner@gmx.de>
2025-11-27 16:42:02 +0100

    fix rendering of styling toggle button

--- webclient/app/src/app/commons/mapMenu/StyledToggleButton.jsx
+++ webclient/app/src/app/commons/mapMenu/StyledToggleButton.jsx
@@ -1,27 +1,27 @@
 import {styled, ToggleButton, Tooltip} from "@mui/material";
 
+const ToggleButtonStyling = styled(ToggleButton)(({theme}) => ({
+    "&.Mui-selected:hover, :hover": {
+        backgroundColor: theme.palette.background.grey
+    },
+    "&.Mui-selected": {
+        backgroundColor: theme.palette.background.green
+    },
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[4],
+    border: "none",
+    color: theme.palette.text.primary
+}));
+
 function StyledToggleButton(props) {
 
     const {children, value, "aria-label": ariaLabel, title, onClick} = props;
 
-    const StyledToggleButton = styled(ToggleButton)(({theme}) => ({
-        "&.Mui-selected:hover, :hover": {
-            backgroundColor: theme.palette.background.grey
-        },
-        "&.Mui-selected": {
-            backgroundColor: theme.palette.background.green
-        },
-        backgroundColor: theme.palette.background.paper,
-        boxShadow: theme.shadows[4],
-        border: "none",
-        color: theme.palette.text.primary
-    }));
-
     return (
         <Tooltip title={title}>
-            <StyledToggleButton value={value} aria-label={ariaLabel} onClick={onClick}>
+            <ToggleButtonStyling value={value} aria-label={ariaLabel} onClick={onClick}>
                 {children}
-            </StyledToggleButton>
+            </ToggleButtonStyling>
         </Tooltip>
     );
 }
