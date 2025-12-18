88780cb0664a19179a58e757837490c32e7078d6
Anett Hübner <anett_huebner@gmx.de>
2025-12-18 14:31:51 +0100

    re-added service tests

--- rest/src/test/java/de/starwit/rest/TestConfig.java
+++ rest/src/test/java/de/starwit/rest/TestConfig.java
@@ -5,11 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 @Configuration
 public class TestConfig {
     @Bean
@@ -20,17 +15,5 @@ public class TestConfig {
 
         restTemplate.setRequestFactory(requestFactory);
         return restTemplate;
-    }
-
-    @Bean
-    public ObjectMapper mapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
-        filterProvider.addFilter("filterName", SimpleBeanPropertyFilter.filterOutAllExcept("name"));
-        filterProvider.addFilter("filterId", SimpleBeanPropertyFilter.filterOutAllExcept("id"));
-        filterProvider.addFilter("filterIdName", SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "title"));
-        mapper.setFilterProvider(filterProvider);
-        return mapper;
     }
 }
