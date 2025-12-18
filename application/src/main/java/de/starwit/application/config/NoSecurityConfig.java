9db098149762340464b710511be2d8c1fb94cd03
Anett Hübner <anett_huebner@gmx.de>
2025-12-18 15:03:25 +0100

    added security

--- application/src/main/java/de/starwit/application/config/NoSecurityConfig.java
+++ application/src/main/java/de/starwit/application/config/NoSecurityConfig.java
@@ -3,9 +3,11 @@ package de.starwit.application.config;
 import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile({ "dev" })
 @Configuration
 public class NoSecurityConfig {
 
