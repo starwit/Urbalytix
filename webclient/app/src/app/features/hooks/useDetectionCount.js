69491430676d03b2005ed02ceedb6a2e5f7237a1
flonix8 <flstanek@googlemail.com>
2026-01-21 17:16:22 +0100

    Fix useDetectionCount() hook missing data caching

--- webclient/app/src/app/features/hooks/useDetectionCount.js
+++ webclient/app/src/app/features/hooks/useDetectionCount.js
@@ -3,29 +3,30 @@ import {FilterContext} from "../../commons/FilterProvider";
 import DetectionCountRest from "../../services/DetectionCountRest";
 
 /**
- * Custom hook to fetch detection data with a limit.
- * @param {number} initialCount - Initial detection count limit.
- * @returns {[Array, number, Function]} [detectionData, detectionCount, setDetectionCount]
+ * Custom hook to fetch detection data based on FilterContext.
+ * @returns {Array} detectionData
  */
 export function useDetectionCount() {
     const {startDate, endDate, selectedObjectClasses} = useContext(FilterContext);
-    const [rawDetectionData, setDetectionData] = useState([]);
+    const [rawDetectionData, setRawDetectionData] = useState([]);
     const detectionCountRest = useMemo(() => new DetectionCountRest(), []);
 
     useEffect(() => {
         detectionCountRest.findByTimeFrame(startDate.toJSON(), endDate.toJSON()).then(response => {
             if (response.data != null) {
-                setDetectionData(response.data);
+                setRawDetectionData(response.data);
             }
         });
     }, [startDate, endDate]);
 
-    var detectionData = rawDetectionData.filter(d => {
-        if (selectedObjectClasses.includes(d.className)) {
-            return true;
-        }
-        return false;
-    });
+    const detectionData = useMemo(() => {
+        return rawDetectionData.filter(d => {
+            if (selectedObjectClasses.includes(d.className)) {
+                return true;
+            }
+            return false;
+        });
+    }, [selectedObjectClasses, rawDetectionData]);
 
     return {
         detectionData
