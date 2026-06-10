8ae8790923de75b621d65f884b9f3e81a093a284
flonix8 <flstanek@googlemail.com>
2026-06-10 17:10:29 +0200

    Auto-load .env file in vite.config; Add variables to configure mock auth

--- webclient/app/vite.config.js
+++ webclient/app/vite.config.js
@@ -1,18 +1,21 @@
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({command}) => {
+
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
   if (command === "serve") {
     return {
       plugins: [
         react(),
-        process.env.NO_AUTH === "true" && {
+        env.MOCK_AUTH === "true" && {
           name: "noauth-mock",
           configureServer(server) {
             server.middlewares.use("/urbalytix/api/user/current", (req, res, next) => {
               if (req.method === "GET") {
                 res.setHeader("Content-Type", "application/json")
-                res.end(JSON.stringify({ authenticated: true, roles: ["admin"]}))
+                res.end(JSON.stringify({ authenticated: true, roles: env.MOCK_AUTH_ROLES?.split(",") || ["admin"] }))
               } else {
                 next() // pass other methods through
               }
